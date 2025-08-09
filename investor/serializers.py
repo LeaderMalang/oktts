06e0ab850fc4e8d1b1377c9009d2fc58dcd39b17
Hassan Ali <hassanali5120@gmail.com>
2025-08-09 21:50:42 +0500

    Add investor list and ledger endpoints

--- investor/serializers.py
+++ investor/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
+from inventory.models import Party
 from .models import InvestorTransaction
 
 
 class InvestorTransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = InvestorTransaction
+        fields = '__all__'
+
+
+class PartySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Party
         fields = '__all__'
