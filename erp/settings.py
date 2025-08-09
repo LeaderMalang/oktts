c134a9731800697bbe854342a602ec6751dcb3d5
Hassan Ali <hassanali5120@gmail.com>
2025-08-09 19:38:50 +0500

    Add drf-spectacular schema generation

--- erp/settings.py
+++ erp/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_spectacular',
     'hr',
     'setting',
     'inventory',
@@ -82,7 +83,8 @@ REST_FRAMEWORK = {
                 'rest_framework.authentication.TokenAuthentication',
                 'rest_framework.authentication.SessionAuthentication',
             ],
-           
+            'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
         }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
