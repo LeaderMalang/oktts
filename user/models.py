a58df393aa2cc2d5cfaa91b18e9090725e0bd443
Hassan Ali <hassanali5120@gmail.com>
2025-08-16 18:57:08 +0500

    Add PasswordResetCode model and migration

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 # accounts/models.py
 from django.contrib.auth.models import AbstractUser,BaseUserManager
 from django.db import models
+from django.utils import timezone
 
 
 
@@ -44,3 +45,16 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
     def __str__(self):
         return f"{self.email} ({self.role})"
+
+
+class PasswordResetCode(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+    def is_expired(self):
+        return timezone.now() >= self.expires_at
+
+    def __str__(self):
+        return f"{self.user.email} - {self.code}"
