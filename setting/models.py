42b6e9f04c8359480c4f0253058f99be09154c49
Hassan Ali <hassanali5120@gmail.com>
2025-08-19 00:16:07 +0500

    Setting module model return name

--- setting/models.py
+++ setting/models.py
@@ -33,6 +33,8 @@ class Company(models.Model):
         blank=True,
         related_name='payroll_payment_company'
     )
+    def __str__(self):
+        return self.name
 
 class Group(models.Model):
     name = models.CharField(max_length=100)
@@ -58,3 +60,5 @@ class Warehouse(models.Model):
     default_purchase_account = models.ForeignKey('voucher.ChartOfAccount', on_delete=models.SET_NULL, null=True, blank=True, related_name='purchase_warehouse')
     default_cash_account = models.ForeignKey('voucher.ChartOfAccount', on_delete=models.SET_NULL, null=True, blank=True, related_name='cash_warehouse')
     default_bank_account = models.ForeignKey('voucher.ChartOfAccount', on_delete=models.SET_NULL, null=True, blank=True, related_name='bank_warehouse')
+    def __str__(self):
+        return self.name
