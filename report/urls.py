8a257fd0309777aa2746081ba4343c5a7942d644
Hassan Ali <hassanali5120@gmail.com>
2025-08-20 04:07:43 +0500

    Add financial statements aggregation service

--- report/urls.py
+++ report/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.report_dashboard, name='report_dashboard'),
+    path("", views.report_dashboard, name="report_dashboard"),
+    path("financial-statement/", views.financial_statement, name="financial_statement"),
 ]
