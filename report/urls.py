e9ce5f724a0deddb7f55b9c3e00438e5f7489c56
Hassan Ali <hassanali5120@gmail.com>
2025-08-20 04:09:02 +0500

    Implement ratio calculation helpers

--- report/urls.py
+++ report/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.report_dashboard, name='report_dashboard'),
+    path('ratios/', views.financial_ratios, name='financial_ratios'),
 ]
--- report/views.py
+++ report/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from voucher.models import VoucherEntry
+from .ratios import current_ratio, gross_profit_margin
 
 
 @require_http_methods(["GET"])
 def report_dashboard(request):
     return render(request, 'report/dashboard.html')
+
+
+@api_view(["GET"])
+def financial_ratios(request):
+    entries = VoucherEntry.objects.all()
+    data = {
+        "currentRatio": current_ratio(entries=entries),
+        "grossProfitMargin": gross_profit_margin(entries=entries),
+    }
+    return Response(data)
