46c25224d5ec28501f00c4d750ccb8ce42287f43
Hassan Ali <hassanali5120@gmail.com>
2025-08-20 03:15:14 +0500

    Add journal voucher type and API

--- voucher/urls.py
+++ voucher/urls.py
@@ -1,12 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ChartOfAccountViewSet, customer_ledger
+from .views import (
+    ChartOfAccountViewSet,
+    JournalVoucherCreateAPIView,
+    customer_ledger,
+)
 
 router = DefaultRouter()
 router.register(r'accounts', ChartOfAccountViewSet, basename='account')
 
 urlpatterns = [
     path('ledger/customer/<int:party_id>/', customer_ledger, name='customer_ledger'),
+    path('journal/', JournalVoucherCreateAPIView.as_view(), name='journal_voucher_create'),
     path('', include(router.urls)),
 ]
