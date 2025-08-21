33fda963b665545ea27c6a7c286d5647c89b15fe
Hassan Ali <hassanali5120@gmail.com>
2025-08-21 22:39:53 +0500

    cros allowed

--- erp/settings.py
+++ erp/settings.py
@@ -27,11 +27,13 @@ SECRET_KEY = 'django-insecure-r3gb32-73qed2#%xux*zc)a)kj2smtnx1d6&t-h%g=!yawq3jj
 DEBUG = True
 ENV= 'live'  # Set to 'production' in production environment
 ALLOWED_HOSTS = ["207.180.252.117", "localhost","127.0.0.1"]
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Add your frontend URL here
     "https://app.okdtts.com/",
 
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 AUTH_USER_MODEL = "user.CustomUser"
 
 # Application definition
